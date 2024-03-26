679aecdc66d24bd997f18543497233c30e145251
unknown <hazelnut41@naver.com>
2024-03-26 17:47:31 +0900

    Update: dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -9,7 +9,7 @@ def load_data():
     df = pd.read_csv("gapminder.tsv", sep="\t")   #sep 은 구분자   표 형태의 data frame이 리턴된다
      return df
 
-def plot_matplotlib():  #streamlit 문법에 맞게 작성을 해주어야함
+def plot_matplotlib():  #streamlit 문법에 맞게 작성을 해주어야함  그래프 삽입하는 메소드
     st.title("Categorical Bar Plot with Seaborn")  #대시보드  제목 
     df = load_data()
     fig, ax = plt.subplots()  # 시각화 하는 메소드
@@ -20,14 +20,15 @@ def plot_matplotlib():  #streamlit 문법에 맞게 작성을 해주어야함
     # Labeling axes and title
     ax.set_xlabel("year")
     ax.set_ylabel("lifeExp")
-    ax.set_title("Categorical Bar Plot")
+    ax.set_title("Year vs.lifeExp")
     
     st.pyplot(fig)  #대시보드에 출력 
 
 def main():
     st.title("Data Display st.dataframe()")
     st.checkbox("Use container width", value=False, key = 'use_container_width')
-    
+
+    st.tite("Maximum value per column")
     df = load_data()
     st.dataframe(df, use_container_width=True)   # 대시보드에 표 삽입 
 
